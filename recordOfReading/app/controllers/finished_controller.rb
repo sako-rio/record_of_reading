827bae92db5fc96c95096dd9ae7f13bc95e10241
sako kazuhiro <sako@rio.ne.jp>
2016-10-12 16:22:56 +0900

    コントローラーとビューの作成

---
+++ recordOfReading/app/controllers/finished_controller.rb
@@ -0,0 +1,7 @@
+class FinishedController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- recordOfReading/config/routes.rb
+++ recordOfReading/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'finished/index'
+
+  get 'finished/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
